35745664d5b794ba5b0c045ffd0c15779b9cb0c5
Gusta-SX <gugaa24@gmail.com>
2022-05-17 14:18:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const div = document.createElement("div");
+div.classList.add("loading-main");
+
+function showLoading() {
+  const loading = document.createElement("div");
+  loading.classList.add("loading");
+  div.appendChild(loading);
+  document.body.appendChild(div);
+
+  setTimeout(() => fetchUsers(), 1000);
+}
+
+function fetchUsers() {
+  if (div.classList.contains("loading-main")) {
+    div.parentNode.removeChild(div);
+    const url = `https://jsonplaceholder.typicode.com/users`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((users) => {
+        const list = document.querySelector(".list");
+        list.classList.add("ativo");
+
+        users.map((user) => {
+          const all = document.createElement("tr");
+          const id = document.createElement("td");
+          const username = document.createElement("td");
+          const name = document.createElement("td");
+          const web = document.createElement("td");
+          const link = document.createElement("a");
+          id.innerHTML = user.id;
+          name.innerHTML = user.name;
+          username.innerHTML = user.username;
+          link.setAttribute("href", user.website);
+          link.innerHTML = user.website;
+          web.appendChild(link);
+          all.appendChild(id);
+          all.appendChild(name);
+          all.appendChild(username);
+          all.appendChild(web);
+          list.appendChild(all);
+        });
+
+        const btn = document.querySelector(".button");
+        btn.disabled = true;
+        btn.style.backgroundColor = '#bc9ede';
+      });
+  }
+}
